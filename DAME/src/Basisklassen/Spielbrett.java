077b687e1edd54a5d9cf868a4a9ad45d2dfbbd16
AdinaLeuchtenberg <Adina_Sofie.Leuchtenberg@Student.Reutlingen-University.DE>
2015-10-13 13:53:48 +0200

    JUnit "soll" Spielbrett erstellt und test implementiert

--- DAME/src/Basisklassen/Spielbrett.java
+++ DAME/src/Basisklassen/Spielbrett.java
@@ -1,12 +1,12 @@
 package Basisklassen;
 
 /**
- * 
- * @author AdinaLeuchtenberg
+ * class Spielbrett
+ * @author The4ofHouseStark
+ * @version 1.0
  *
  */
 public class Spielbrett {
-
 	
 	
 }
--- DAME/src/JunitTests/SpielbrettJunitTest.java
+++ DAME/src/JunitTests/SpielbrettJunitTest.java
@@ -2,35 +2,54 @@ package JunitTests;
 
 import static org.junit.Assert.*;
 
+import java.lang.reflect.Array;
+
 import org.junit.Before;
 import org.junit.Test;
 
 import Basisklassen.Spielbrett;
+import Basisklassen.Spielfeld;
 
 
 /**
  * Junit Testclass for the gaming board
- * @author AdinaLeuchtenberg
+ * @author The4ofHouseStark
  * @version 1.0
  *
  */
 public class SpielbrettJunitTest {
 	
 	private Spielbrett board;
+	private Array[][] testBoard;
 	
 	/**
-	 * create a new board object before starting the Junit test
+	 * creates a new board object before starting the Junit test
 	 */
 	
 	@Before
 	public void newBoard() {
 		board = new Spielbrett();
 	}
+	
+	/**
+	 * creates a board how it should be during the game
+	 */
+	@Before
+	public void testBoard() {
+		testBoard = new Array[12][12];
+		//toDo richtige ids übergeben
 
+	}
+
+	/**
+	 * Test to check if the board has valid Ids
+	 */
 	@Test
-	public void test() {
+	public void rightFieldId() {
+		Array[][] fieldId = board.getBoard();
 		
-		fail("Not yet implemented");
+		assertArrayEquals(fieldId, testBoard);
+		
 	}
 
 }
