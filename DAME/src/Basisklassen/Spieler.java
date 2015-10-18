db3c61ddbf2944bb6dfcb6e12a16dd8e01abcb8a
TamaraBerner <Tamara.Berner@student.reutlingen-university.de>
2015-10-18 14:44:58 +0200

    Hinzufügen JavaDoc und import ExtraKlassen.FarbEnum in Spieler Klasse

--- DAME/src/Basisklassen/Spieler.java
+++ DAME/src/Basisklassen/Spieler.java
@@ -1,6 +1,7 @@
 package Basisklassen;
 
 import java.util.ArrayList;
+import ExtraKlassen.FarbEnum;
 
 /*
  * class Spieler
@@ -14,7 +15,7 @@ public class Spieler {
 	private String spielerName;
 	private FarbEnum farbenEnum;
 	private ArrayList AnzPlayer = new ArrayList();
-	
+	//
 	public String getSpielerName() {
 		return spielerName;
 	}
@@ -23,16 +24,25 @@ public class Spieler {
 		this.spielerName = n;
 	}
 	
+	/**
+	 *@param adds player,spielerName to AnzPlayer ArrayList
+	 */
+	
 	public void addPlayer(){
 		AnzPlayer.add(getSpielerName());
 		AnzPlayer.add(1, getSpielerName());
 	}
 	
+	/**
+	 * Method PlayerColour() 
+	 * @param Player1 gets the color white, next player gets the color black out of farbenEnum
+	 */
+	
   public void PlayerColour (){
   	if(AnzPlayer.contains(getSpielerName())){
-  		farbenEnum = farbenEnum.black;
+  		farbenEnum = farbenEnum.white;
   				
-  	}else farbenEnum = farbenEnum.white;
+  	}else farbenEnum = farbenEnum.black;
   
   }
 }
