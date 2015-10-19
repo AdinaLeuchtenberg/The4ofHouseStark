80a7c039ea8e53a444ea1def011197482bd61f39
Manuel Kelch <manuel.kelch@student.reutlingen-university.com>
2015-10-19 21:17:44 +0200

    J-Unit Klasse für Spielfeld verbessert

--- DAME/src/JunitTests/SpielfeldJunitTest.java
+++ DAME/src/JunitTests/SpielfeldJunitTest.java
@@ -1,15 +1,33 @@
 package JunitTests;
 
 import static org.junit.Assert.*;
+/**
+ * Junit Testclass for the player
+ * @author The4ofHouseStark
+ * @version 1.0
+ *
+ */
+
 import Basisklassen.Spielfeld;
 import ExtraKlassen.FarbEnum;
 import org.junit.Before;
 import org.junit.Test;
 
 public class SpielfeldJunitTest {
-	protected Spielfeld spielfeld;
-	Spielfeld A1 = new Spielfeld(FarbEnum.black,"A1");
+	public Spielfeld spielfeld;
+	Spielfeld A1;
+	/**
+	 * creates a new Spielfeld before starting the Junit test
+	 */
 	
+	@Before
+	public void Spielfelderzeugen(){;
+	Spielfeld A2 = new Spielfeld(FarbEnum.black,"A2");
+	this.A1=A2;
+	}
+	/**
+	 * test to check if constructor works right for the color
+	 */
 	@Test
 	public void test(){
 		
