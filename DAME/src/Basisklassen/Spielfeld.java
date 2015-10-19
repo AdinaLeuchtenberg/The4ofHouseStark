28ee894d6b93490a077af45f614bd50fa874d747
ManuelKelch <manuel.kelch@student.reutlingen-university.de>
2015-10-19 16:07:48 +0200

    Spielfeld Klasse gecoded

--- DAME/src/Basisklassen/Spielfeld.java
+++ DAME/src/Basisklassen/Spielfeld.java
@@ -1,9 +1,21 @@
 package Basisklassen;
 
+import ExtraKlassen.FarbEnum;
+
 public class Spielfeld {
+public Spielfigur spielfigur;
+String ID = "";
+FarbEnum color;
 
-	public static void main(String[] args) {
-		System.out.println("test");
-	}
+public Spielfeld(FarbEnum y,String x){
+	this.color = y;
+	this.ID = x;
+}
 
+public String getID(){
+	return this.ID;
+}
+public FarbEnum getColor(){
+	return this.color;
+}
 }
