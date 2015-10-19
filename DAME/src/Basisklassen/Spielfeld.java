9d2f695aeac116543ffe856aa181f0b1e3593d07
Manuel Kelch <manuel.kelch@student.reutlingen-university.com>
2015-10-19 18:42:53 +0200

    Java Doc bei Spielfeld hinzugefügt

--- DAME/src/Basisklassen/Spielfeld.java
+++ DAME/src/Basisklassen/Spielfeld.java
@@ -1,12 +1,21 @@
 package Basisklassen;
 
 import ExtraKlassen.FarbEnum;
+/**
+ * class Spielfeld
+ * @author The4ofHouseStark
+ * @version 1.0
+ *
+ */
 
 public class Spielfeld {
 public Spielfigur spielfigur;
 String ID = "";
 FarbEnum color;
 
+/**
+ * constructor sets color, ID
+ */
 public Spielfeld(FarbEnum y,String x){
 	this.color = y;
 	this.ID = x;
