2a1a1d7ff0ec704f9ba5a2057693812f02bfbc15
RoxanneAdler <Roxanne.Adler@Student.Reutlingen-University.DE>
2015-10-25 19:27:08 +0100

    Spiel move unfertig, rest passt soweit

--- DAME/src/SpielKlassen/Spiel.java
+++ DAME/src/SpielKlassen/Spiel.java
@@ -7,9 +7,11 @@ import ExtraKlassen.FarbEnum;
 
 public class Spiel implements iBediener {
 
+
 	private Spielbrett map;
 	private Spielfigur token;
 	private PlayerEnum player;
+	private FarbEnum [] TokenArray = new FarbEnum[48];
 	private enum PlayerEnum{
 		ONE,TWO;
 	}
@@ -18,15 +20,61 @@ public class Spiel implements iBediener {
 		map = new Spielbrett();
 		
 	}
+	/**
+	* 
+	* @return return colours of tokens
+*/
+public FarbEnum[] getFarbTokenArray(){
 	
-	public String move(boolean dame, String position, Spielbrett map){
+	for(int i=0; i<(TokenArray.length/2); i++){
+		TokenArray[i] = FarbEnum.white;
+	} 
+	for(int j=24;j<TokenArray.length; j++){
+		TokenArray[j]= FarbEnum.black;
+	}
+	return TokenArray;
+}
+	
+	public String move(boolean dame, String position, String iDFeld){
 		dame = false;
+		int [] posNow = map.idToIndex(position);
+		int [] posNext = map.idToIndex(iDFeld);
 		
-		if(){
+		// für weißer spieler
+		if(map.getField([posNext[0]][posNext[1]]).getColor() == FarbEnum.black){ 	// farb test 
+			if(posNext[0]<= 11 && posNext[0]>=0){ // im Feld (buchstaben)
+				if(posNext[1]<= 11 && posNext[1]>=0){ // im feld (zahlen)
+					if(posNext[0]== posNow[0]+1){ // ein feld weiter (nach oben)
+						if(posNext[1]==posNow[1]+1 || posNext[1]== posNow[1]-1){ // Feld nach rechts oder links 
+							if(map.getField([posNext[0]][posNext[1]]).fieldBesetzt()){ //Feld besetzt?
+								if(map.getField([posNext[0]][posNext[1]]).getSpielfigur.getFarbenEnum()== FarbEnum.black ){	// Ist Spielfigur schwarz somit Gegnerfarbe
+									if(posNext[0]-1 <= 11 && posNext[0]-1 >=0){	// Ist neues Spielfeld im Feld, nächste reihe 
+										if((posNext[1]+1 <= 11 && posNext[1]+ 1 >=0)||(posNext[1]-1 <= 11 && posNext[1]- 1 >=0)){ // NeuesSpielfeld ist im Brett um schlagen möglich zu machen 
+											if((map.getField([posNext[0]+1][posNext[1]+1]).fieldBesetzt()) || (map.getField([posNext[0]+1][posNext[1]-1]).fieldBesetzt())){
+												map.getField([posNext[0]][posNext[1]]).setSpielfigur(null);	// setzt das Feld null auf dem die Spielfigur jetzt saß
+													int j=24;
+													while(TokenArray[j]= null){		//löscht den Token aus den FarbTokenArray
+														j++;
+													} TokenArray[j] =null;
+													 //... setzt die Spielfigur aufs neue Feld + muss schlagen wenn möglich!!
+													
+												}
+												
+											}
+										}
+									}
+								}
+							}
+							
+						}
+					}
+				}
+				
+			}
 			
 			
 		}
-		return Spielfigur.setPosition(iD);
+		return ;
 	}
 	
 	private void changePlayer(){
@@ -39,5 +87,10 @@ public class Spiel implements iBediener {
 		else{
 			System.out.println("Error: no player selected");
 		}
+	}
+	@Override
+	public void reader() {
+		// TODO Auto-generated method stub
+		
 	}
 }
