63d6298da29008ddada1dd8a34880332d8d4cd3d
RoxanneAdler <Roxanne.Adler@student.reutlingen-university.de>
2015-10-26 18:53:11 +0100

    move geändert // test

---
+++ DAME/src/JunitTests/SpielJunitTest.java
@@ -0,0 +1,15 @@
+package JunitTests;
+
+import static org.junit.Assert.*;
+
+/**
+ * Junit Testclass for Spieler
+ * @author The4ofHouseStark
+ * @version 1.0
+ *
+ */
+
+public class SpielJunitTest {
+	
+	
+}
