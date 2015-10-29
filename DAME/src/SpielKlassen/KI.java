219324e7bfc47cddc135944e985d9accfbfffd30
AdinaLeuchtenberg <Adina_Sofie.Leuchtenberg@Student.Reutlingen-University.DE>
2015-10-29 15:41:50 +0100

    csv in spiel gelöscht, KI dame struktur

--- DAME/src/SpielKlassen/KI.java
+++ DAME/src/SpielKlassen/KI.java
@@ -1,9 +1,42 @@
 package SpielKlassen;
 
+import Basisklassen.Spieler;
+
 public abstract class KI {
 
-	public static void main(String[] args) {
-
+	private Spieler spieler;
+	
+	public KI(Spieler spieler) {
+		this.spieler = spieler;	
+	}
+	
+	/**
+	 * Goal of the KI to reach the other end of the board
+	 */
+	public void zurDameWerden() {
+		
+	}
+	
+	/**
+	 * goal of the KI to take away the tokens of the other player
+	 */
+	public void schlagen() {
+		
+	}
+	
+	/**
+	 * goal of the KI to keep tokens
+	 */
+	public void selbstNichtGeschlagenWerden() {
+		
+	}
+	
+	/**
+	 * goal of the KI to prevent that the other player gets a "Dame"
+	 */
+	
+	public void keineDameDesGegnersZulassen() {
+		
 	}
 
 }
