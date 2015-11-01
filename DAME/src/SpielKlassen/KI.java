797fd29f7289956e8b2fbc2be70d16bf0180c4fe
AdinaLeuchtenberg <Adina_Sofie.Leuchtenberg@Student.Reutlingen-University.DE>
2015-11-01 18:50:51 +0100

    keineDameDesGegnersZulassen in KI geändert

--- DAME/src/SpielKlassen/KI.java
+++ DAME/src/SpielKlassen/KI.java
@@ -13,161 +13,178 @@ public abstract class KI {
 	private Spiel spiel = new Spiel();
 	private Spielbrett board = spiel.getMap();
 	private Spielfigur token;
-	
+
 	public KI(Spieler spieler) {
-		this.spieler = spieler;	
+		this.spieler = spieler;
 	}
-	
+
 	/**
-	 *Goal of the KI to reach the other end of the board
-	 *@return returns true if there is no token to beat
-	 *returns false if there is one (schlagen?)
+	 * Goal of the KI to reach the other end of the board
+	 *
+	 * @return returns true if there is no token to beat returns false if there is
+	 *         one (schlagen?)
 	 */
 	public boolean zurDameWerden() {
-		//TODO Tamara
-				if(board.getFigurenDieSchlagenKoennen()==null){
-					String positionSpielfigur = token.getPosition();
-					if(token.getFarbenEnum() == FarbEnum.black) {
-						//choose a token which is the closest to index 11
-						return true;
-					}
-					if(token.getFarbenEnum() == FarbEnum.white) {
-						//choose a token which is the closest to index 0
-						return true;
-					}
-					
-					
-				}return false; //schlagen()?
+		// TODO Tamara
+		if (board.getFigurenDieSchlagenKoennen() == null) {
+			String positionSpielfigur = token.getPosition();
+			if (token.getFarbenEnum() == FarbEnum.black) {
+				// choose a token which is the closest to index 11
+				return true;
 			}
-			
+			if (token.getFarbenEnum() == FarbEnum.white) {
+				// choose a token which is the closest to index 0
+				return true;
+			}
+
+		}
+		return false; // schlagen()?
+	}
+
 	/**
 	 * goal of the KI to take away the tokens of the other player(random)
-	 * @return returns true if the KI can take one or more tokens of the other player
-	 * return false if there is no token to beat
+	 * 
+	 * @return returns true if the KI can take one or more tokens of the other
+	 *         player return false if there is no token to beat
 	 */
 	public boolean schlagen() {
-		//TODO Tamara
-		for(int i = 0; i < 12; i++) {
-			if(board.getFigurenDieSchlagenKoennen()!=null){
-			Spielfigur figurDieSchlagenKann = board.getFigurenDieSchlagenKoennen().get(i);
-				if(figurDieSchlagenKann.getFarbenEnum() == FarbEnum.black) {
-					//int Spielfigur[(new Random()).nextInt(Spielfigur.length)];
+		// TODO Tamara
+		for (int i = 0; i < 12; i++) {
+			if (board.getFigurenDieSchlagenKoennen() != null) {
+				Spielfigur figurDieSchlagenKann = board.getFigurenDieSchlagenKoennen().get(i);
+				if (figurDieSchlagenKann.getFarbenEnum() == FarbEnum.black) {
+					// int Spielfigur[(new Random()).nextInt(Spielfigur.length)];
 					return true;
-					
-			} else if(figurDieSchlagenKann.getFarbenEnum() == FarbEnum.white) {
-						//int Spielfigur[(new Random()).nextInt(Spielfigur.length)];
-						return true;
-					}
+
+				} else if (figurDieSchlagenKann.getFarbenEnum() == FarbEnum.white) {
+					// int Spielfigur[(new Random()).nextInt(Spielfigur.length)];
+					return true;
 				}
-			
-			}	return false;
+			}
+
 		}
-	
+		return false;
+	}
+
 	/**
 	 * goal of the KI to keep tokens
 	 */
 	public boolean selbstNichtGeschlagenWerden(int[] posNext) {
-			// test auf Dame rechts hoch 
-		for(int i=2;i<12;i++){//von 2 über dem feld bis zum max. letzten feld auf dame testen
-			if(board.getField(posNext[0]+i, posNext[1]+i).fieldBesetzt()){
-				if(board.getField(posNext[0]+i, posNext[1]+i).getSpielfigur().getDame()){
-					if(board.getField(posNext[0]-1, posNext[1]-1).fieldBesetzt()){
-				return false;
-				}
-				}
-			}
-		}
-		// test auf Dame links hoch
-		for(int i=2;i<12;i++){//von 2 über dem feld bis zum max. letzten feld auf dame testen
-			if(board.getField(posNext[0]+i, posNext[1]-i).fieldBesetzt()){
-				if(board.getField(posNext[0]+i, posNext[1]-i).getSpielfigur().getDame()){
-					if(board.getField(posNext[0]-1, posNext[1]+1).fieldBesetzt()){
-				return false;
-				}
-			}
-		}
-		}
-		// test auf Dame rechts runter
-		for(int i=2;i<12;i++){//von 2 über dem feld bis zum max. letzten feld auf dame testen
-			if(board.getField(posNext[0]-i, posNext[1]+i).fieldBesetzt()){
-				if(board.getField(posNext[0]-i, posNext[1]+i).getSpielfigur().getDame()){
-					if(board.getField(posNext[0]+1, posNext[1]-1).fieldBesetzt()){
-				return false;
-				}
-			}
-		}
-	}
-		// test auf Dame links runter
-		for(int i=2;i<12;i++){//von 2 über dem feld bis zum max. letzten feld auf dame testen
-		if(board.getField(posNext[0]-i, posNext[1]-i).fieldBesetzt()){
-			if(board.getField(posNext[0]-i, posNext[1]-i).getSpielfigur().getDame()){
-				if(board.getField(posNext[0]+1, posNext[1]+1).fieldBesetzt()){
-			return false;
-			}
-		}
-	}
-}
-		if(spieler.getFarbEnum()==FarbEnum.white){//für weißen spieler
-			//rechts oben über zielfeld steht gegner, ist feld unten links von zielfeld frei
-			if(board.getField(posNext[0]+1,posNext[1]+1).fieldBesetzt()){
-				if(board.getField(posNext[0]+1,posNext[1]+1).getSpielfigur().getFarbenEnum() == FarbEnum.black){
-					if(!board.getField(posNext[0]-1,posNext[1]-1).fieldBesetzt()){
+		// test auf Dame rechts hoch
+		for (int i = 2; i < 12; i++) {// von 2 über dem feld bis zum max. letzten
+																	// feld auf dame testen
+			if (board.getField(posNext[0] + i, posNext[1] + i).fieldBesetzt()) {
+				if (board.getField(posNext[0] + i, posNext[1] + i).getSpielfigur().getDame()) {
+					if (board.getField(posNext[0] - 1, posNext[1] - 1).fieldBesetzt()) {
 						return false;
 					}
 				}
 			}
-		//links oben über zielfeld steht gegner, ist feld unten rechts von zielfeld frei
-				if(board.getField(posNext[0]+1,posNext[1]-1).fieldBesetzt()){
-					if(board.getField(posNext[0]+1,posNext[1]-1).getSpielfigur().getFarbenEnum() == FarbEnum.black){
-						if(!board.getField(posNext[0]-1,posNext[1]+1).fieldBesetzt()){
-							return false;
-						}
+		}
+		// test auf Dame links hoch
+		for (int i = 2; i < 12; i++) {// von 2 über dem feld bis zum max. letzten
+																	// feld auf dame testen
+			if (board.getField(posNext[0] + i, posNext[1] - i).fieldBesetzt()) {
+				if (board.getField(posNext[0] + i, posNext[1] - i).getSpielfigur().getDame()) {
+					if (board.getField(posNext[0] - 1, posNext[1] + 1).fieldBesetzt()) {
+						return false;
 					}
-				}return true;
-		}
-	if(spieler.getFarbEnum()== FarbEnum.black){
-	//für schwarzen spieler
-//rechts unten unter zielfeld steht gegner, ist feld oben	links von zielfeld frei
-	if(board.getField(posNext[0]-1,posNext[1]+1).fieldBesetzt()){
-		if(board.getField(posNext[0]-1,posNext[1]+1).getSpielfigur().getFarbenEnum() == FarbEnum.black){
-			if(!board.getField(posNext[0]+1,posNext[1]-1).fieldBesetzt()){
-				return false;
-			}
-		}
-	}
-	//für schwarzen spieler
-	//links unten unter zielfeld steht gegner, ist feld oben	rechts von zielfeld frei
-		if(board.getField(posNext[0]-1,posNext[1]-1).fieldBesetzt()){
-			if(board.getField(posNext[0]-1,posNext[1]-1).getSpielfigur().getFarbenEnum() == FarbEnum.black){
-				if(!board.getField(posNext[0]+1,posNext[1]+1).fieldBesetzt()){
-					return false;
 				}
 			}
 		}
+		// test auf Dame rechts runter
+		for (int i = 2; i < 12; i++) {// von 2 über dem feld bis zum max. letzten
+																	// feld auf dame testen
+			if (board.getField(posNext[0] - i, posNext[1] + i).fieldBesetzt()) {
+				if (board.getField(posNext[0] - i, posNext[1] + i).getSpielfigur().getDame()) {
+					if (board.getField(posNext[0] + 1, posNext[1] - 1).fieldBesetzt()) {
+						return false;
+					}
+				}
+			}
+		}
+		// test auf Dame links runter
+		for (int i = 2; i < 12; i++) {// von 2 über dem feld bis zum max. letzten
+																	// feld auf dame testen
+			if (board.getField(posNext[0] - i, posNext[1] - i).fieldBesetzt()) {
+				if (board.getField(posNext[0] - i, posNext[1] - i).getSpielfigur().getDame()) {
+					if (board.getField(posNext[0] + 1, posNext[1] + 1).fieldBesetzt()) {
+						return false;
+					}
+				}
+			}
+		}
+		if (spieler.getFarbEnum() == FarbEnum.white) {// für weißen spieler
+			// rechts oben über zielfeld steht gegner, ist feld unten links von
+			// zielfeld frei
+			if (board.getField(posNext[0] + 1, posNext[1] + 1).fieldBesetzt()) {
+				if (board.getField(posNext[0] + 1, posNext[1] + 1).getSpielfigur().getFarbenEnum() == FarbEnum.black) {
+					if (!board.getField(posNext[0] - 1, posNext[1] - 1).fieldBesetzt()) {
+						return false;
+					}
+				}
+			}
+			// links oben über zielfeld steht gegner, ist feld unten rechts von
+			// zielfeld frei
+			if (board.getField(posNext[0] + 1, posNext[1] - 1).fieldBesetzt()) {
+				if (board.getField(posNext[0] + 1, posNext[1] - 1).getSpielfigur().getFarbenEnum() == FarbEnum.black) {
+					if (!board.getField(posNext[0] - 1, posNext[1] + 1).fieldBesetzt()) {
+						return false;
+					}
+				}
+			}
+			return true;
+		}
+		if (spieler.getFarbEnum() == FarbEnum.black) {
+			// für schwarzen spieler
+			// rechts unten unter zielfeld steht gegner, ist feld oben links von
+			// zielfeld frei
+			if (board.getField(posNext[0] - 1, posNext[1] + 1).fieldBesetzt()) {
+				if (board.getField(posNext[0] - 1, posNext[1] + 1).getSpielfigur().getFarbenEnum() == FarbEnum.black) {
+					if (!board.getField(posNext[0] + 1, posNext[1] - 1).fieldBesetzt()) {
+						return false;
+					}
+				}
+			}
+			// für schwarzen spieler
+			// links unten unter zielfeld steht gegner, ist feld oben rechts von
+			// zielfeld frei
+			if (board.getField(posNext[0] - 1, posNext[1] - 1).fieldBesetzt()) {
+				if (board.getField(posNext[0] - 1, posNext[1] - 1).getSpielfigur().getFarbenEnum() == FarbEnum.black) {
+					if (!board.getField(posNext[0] + 1, posNext[1] + 1).fieldBesetzt()) {
+						return false;
+					}
+				}
+			}
+		}
+		return true;
 	}
-	return true;
-	}
-	
+
 	/**
 	 * goal of the KI to prevent that the other player gets a "Dame"
-	 * @return returns true if KI can take a token of the other player to prevent that he gets a "Dame" 
-	 * return false if not
+	 * 
+	 * @return returns true if KI can take a token of the other player to prevent
+	 *         that he gets a "Dame" return false if not
 	 */
-	
+
 	public boolean keineDameDesGegnersZulassen() {
-		for(int i = 0; i < 12; i++) {
+		for (int i = 0; i < 12; i++) {
 			Spielfigur figurDieSchlagenKann = board.getFigurenDieSchlagenKoennen().get(i);
 			int[] positionFigurDieSchlagenKann = board.idToIndex(figurDieSchlagenKann.getPosition());
-			if(figurDieSchlagenKann.getFarbenEnum() == FarbEnum.black) {
-				if(positionFigurDieSchlagenKann[0] < 4) {
-					 return true;
-				}
-			} else if(figurDieSchlagenKann.getFarbenEnum() == FarbEnum.white) {
-					if(positionFigurDieSchlagenKann[0] > 7) {
+			if (spiel.getCurrentPlayer().getFarbEnum() == FarbEnum.black) {
+				if (figurDieSchlagenKann.getFarbenEnum() == FarbEnum.black) {
+					if (positionFigurDieSchlagenKann[0] < 4) {
 						return true;
 					}
 				}
-			
-			}	return false;
+			} else {
+				if (figurDieSchlagenKann.getFarbenEnum() == FarbEnum.white) {
+					if (positionFigurDieSchlagenKann[0] > 7) {
+						return true;
+					}
+				}
+			}
 		}
+		return false;
+	}
 }
